0bc644124f82cdb4763c8658ef67fd1a575d8d7f
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-05-06 15:28:15 +0530

    completed DemoTypecasting

--- Practice_Project-1/DemoTypeCasting.java
+++ Practice_Project-1/DemoTypeCasting.java
@@ -1,9 +1,24 @@
 /**
  * DemoTypeCasting
+ * Writing a program in Java to implement implicit and explicit type casting
  */
 public class DemoTypeCasting {
     public static void main(String[] args) {
-        System.out.println("Hello ");
+
+        // Implicit type casting - automatically done by compiler
+        char a = 'y';
+
+        int c = a + 12; // a that stores 'y' is automatically converted into integer
+        System.out.println("Before Implicit type-casting : " + a);
+        System.out.println("After Implicit type-casting : " + c);
+
+        // Explicit type casting - user have to do manually
+        double n = 12.34;
+        int x = (int) n;
+
+        System.out.println("Before explicit type-casting : " + n);
+        System.out.println("After explicit type-casting : " + x);
+
     }
 
 }
