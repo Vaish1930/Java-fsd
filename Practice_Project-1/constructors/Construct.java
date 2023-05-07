f656f39ba508d22252945c82a3217e0d74a87362
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-05-07 20:28:55 +0530

    Implemented constructors

---
+++ Practice_Project-1/constructors/Construct.java
@@ -0,0 +1,29 @@
+package constructors;
+
+//  Three types of constructors in java
+// 1. Default constructor
+// 2.Parameterized constructor
+
+public class Construct {
+
+    int a;
+    int b;
+
+    Construct() {
+        System.out.println("Default constructor called\n");
+    }
+
+    Construct(int a, int b) {
+        System.out.println("Parameterized constructor called");
+
+        this.a = a;
+        this.b = b;
+    }
+
+    public static void main(String[] args) {
+        new Construct();
+        var construct = new Construct(12, 14);
+        System.out.println("a :" + construct.a);
+        System.out.println("b :" + construct.b);
+    }
+}
