87b5af0b1f462bf2a5816a0bb894380e38378d23
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-05-16 13:03:24 +0530

    Sum of elemets in the given range

--- Practice_Project-3/Range.java
+++ Practice_Project-3/Range.java
@@ -2,22 +2,18 @@ import java.util.Scanner;
 
 public class Range {
 
-    static int sum(int n) {
-        int sum = (n * (n + 1)) / 2;
-        return sum;
-    }
-
-    static int sumInRange(int l, int r) {
-        return sum(r) - sum(l - 1);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input range");
         int l = sc.nextInt();
         int r = sc.nextInt();
+        int sum = 0;
 
-        System.out.println("Sum of Natural numbers from L to R is " + sumInRange(l, r));
+        for (int i = l; i <= r; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of Natural numbers from L to R is " + sum);
         sc.close();
     }
 }
