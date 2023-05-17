e4d772ac59f858557c2e8fb2b5e9314c9fb1bd5b
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-05-17 22:11:32 +0530

    Implemented Insertion and Selection Sort

--- practice_project_4/InsertionSort.java
+++ practice_project_4/InsertionSort.java
@@ -1,5 +1,43 @@
 package practice_project_4;
 
-public class InsertionSort {
+import java.util.Scanner;
 
+public class InsertionSort {
+    public static void insertionSort(int array[]) {
+        int n = array.length;
+        for (int j = 1; j < n; j++) {
+            int key = array[j];
+            int i = j - 1;
+            while ((i > -1) && (array[i] > key)) {
+                array[i + 1] = array[i];
+                i--;
+            }
+            array[i + 1] = key;
+        }
+    }
+
+    public static void main(String a[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input size of array");
+        int size = sc.nextInt();
+        int ar[] = new int[size];
+
+        System.out.println("Input numbers in Array");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+        System.out.println("Before Insertion Sort");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        insertionSort(ar);// sorting array using insertion sort
+
+        System.out.println("After Insertion Sort");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(i + " ");
+        }
+        sc.close();
+    }
 }
--- practice_project_4/SelectionSort.java
+++ practice_project_4/SelectionSort.java
@@ -1,5 +1,44 @@
 package practice_project_4;
 
-public class SelectionSort {
+import java.util.Scanner;
 
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+    }
+
+    public static void main(String a[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input size of array");
+        int size = sc.nextInt();
+        int ar[] = new int[size];
+
+        System.out.println("Input numbers in Array");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+        System.out.println("Before Selection Sort");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        selectionSort(ar);
+
+        System.out.println("After Selection Sort");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(i + " ");
+        }
+        sc.close();
+    }
 }
