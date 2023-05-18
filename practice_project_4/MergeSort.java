2b9c53d8f37936d8f9de4a98d5a2f85730511924
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-05-18 10:15:46 +0530

    Implemented merge and quick sort

--- practice_project_4/MergeSort.java
+++ practice_project_4/MergeSort.java
@@ -1,5 +1,82 @@
 package practice_project_4;
 
-public class MergeSort {
+import java.util.Scanner;
 
+public class MergeSort {
+    void merge(int ar[], int beg, int mid, int end) {
+        int i, j, k;
+        int n1 = mid - beg + 1;
+        int n2 = end - mid;
+
+        int LeftArray[] = new int[n1];
+        int RightArray[] = new int[n2];
+
+        /* copy data to temp arrays */
+        for (i = 0; i < n1; i++)
+            LeftArray[i] = ar[beg + i];
+        for (j = 0; j < n2; j++)
+            RightArray[j] = ar[mid + 1 + j];
+
+        i = 0;
+        j = 0;
+        k = beg;
+
+        while (i < n1 && j < n2) {
+            if (LeftArray[i] <= RightArray[j]) {
+                ar[k] = LeftArray[i];
+                i++;
+            } else {
+                ar[k] = RightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            ar[k] = LeftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            ar[k] = RightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(int ar[], int beg, int end) {
+        if (beg < end) {
+            int mid = (beg + end) / 2;
+            mergeSort(ar, beg, mid);
+            mergeSort(ar, mid + 1, end);
+            merge(ar, beg, mid, end);
+        }
+    }
+
+    void displayArray(int ar[], int size) {
+        int i;
+        for (i = 0; i < size; i++)
+            System.out.print(ar[i] + " ");
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input size of array");
+        int size = sc.nextInt();
+        int ar[] = new int[size];
+
+        System.out.println("Input numbers in Array");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+        MergeSort mergeSorting = new MergeSort();
+        System.out.println("\nBefore sorting array elements are - ");
+        mergeSorting.displayArray(ar, size);
+        mergeSorting.mergeSort(ar, 0, size - 1);
+        System.out.println("\nAfter sorting array elements are - ");
+        mergeSorting.displayArray(ar, size);
+        System.out.println("");
+
+        sc.close();
+    }
 }
--- practice_project_4/QuickSort.java
+++ practice_project_4/QuickSort.java
@@ -1,5 +1,59 @@
 package practice_project_4;
 
-public class QuickSort {
+import java.util.Scanner;
 
+public class QuickSort {
+    int partition(int ar[], int start, int end) {
+        int pivot = ar[end]; // pivot element
+        int i = (start - 1);
+
+        for (int j = start; j <= end - 1; j++) {
+
+            if (ar[j] < pivot) {
+                i++;
+                int t = ar[i];
+                ar[i] = ar[j];
+                ar[j] = t;
+            }
+        }
+        int t = ar[i + 1];
+        ar[i + 1] = ar[end];
+        ar[end] = t;
+        return (i + 1);
+    }
+
+    void quick(int ar[], int start, int end) {
+        if (start < end) {
+            int p = partition(ar, start, end);
+            quick(ar, start, p - 1);
+            quick(ar, p + 1, end);
+        }
+    }
+
+    void displayArray(int ar[], int size) {
+        int i;
+        for (i = 0; i < size; i++)
+            System.out.print(ar[i] + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input size of array");
+        int size = sc.nextInt();
+        int ar[] = new int[size];
+
+        System.out.println("Input numbers in Array");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+
+        QuickSort quickSort = new QuickSort();
+        System.out.println("\nBefore sorting array elements are - ");
+        quickSort.displayArray(ar, size);
+        quickSort.quick(ar, 0, size - 1);
+        System.out.println("\nAfter sorting array elements are - ");
+        quickSort.displayArray(ar, size);
+        System.out.println();
+        sc.close();
+    }
 }
