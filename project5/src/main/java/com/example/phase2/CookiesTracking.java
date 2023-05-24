fb8cb9b8f2a7c1442f6a1df7a4e8c915a8df7de4
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-05-24 13:06:03 +0530

    Implemented Session Tracking

---
+++ project5/src/main/java/com/example/phase2/CookiesTracking.java
@@ -0,0 +1,94 @@
+package com.example.phase2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class CookiesTracking extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
+
+        Date createTime = new Date(session.getCreationTime());
+
+        Date lastAccessTime = new Date(session.getLastAccessedTime());
+
+        String title = "Welcome Back to Session";
+        int visitCount = new Integer(0);
+        String visitCountKey = new String("visitCount");
+        String userIDKey = new String("userID");
+        String userID = new String("Vaishnavi");
+
+        if (session.isNew()) {
+            title = "Welcome to Session";
+            session.setAttribute(userIDKey, userID);
+        } else {
+            visitCount = (Integer) (session.getAttribute(
+                    visitCountKey) == null ? 0 : session.getAttribute(visitCountKey));
+
+            visitCount = visitCount + 1;
+            userID = (String) session.getAttribute(userIDKey);
+        }
+        session.setAttribute(visitCountKey, visitCount);
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 "
+                + "transitional//en\">\n";
+
+        out.println(
+                docType + "<html>\n"
+                        + "<head><title>" + title + "</title></head>\n"
+                        +
+
+                        "<body bgcolor = \"#f0f0f0\">\n"
+                        + "<h1 align = \"center\">" + title + "</h1>\n"
+                        + "<h2 align = \"center\">Session Information</h2>\n"
+                        + "<table border = \"1\" align = \"center\">\n"
+                        +
+
+                        "<tr bgcolor = \"#949494\">\n"
+                        + "  <th>Session info</th><th>value</th>"
+                        + "</tr>\n"
+                        +
+
+                        "<tr>\n"
+                        + "  <td>id</td>\n"
+                        + "  <td>" + session.getId() + "</td>"
+                        + "</tr>\n"
+                        +
+
+                        "<tr>\n"
+                        + "  <td>Creation Time</td>\n"
+                        + "  <td>" + createTime + "  </td>"
+                        + "</tr>\n"
+                        +
+
+                        "<tr>\n"
+                        + "  <td>Time of Last Access</td>\n"
+                        + "  <td>" + lastAccessTime + "</td>"
+                        + "</tr>\n"
+                        +
+
+                        "<tr>\n"
+                        + "  <td>User ID</td>\n"
+                        + "  <td>" + userID + "</td>"
+                        + "</tr>\n"
+                        +
+
+                        "<tr>\n"
+                        + "  <td>Number of visits</td>\n"
+                        + "  <td>" + visitCount + "</td>"
+                        + "</tr>\n"
+                        + "</table>\n"
+                        + "</body>"
+                        + "</html>");
+    }
+}
--- project5/src/main/webapp/index.jsp
+++ project5/src/main/webapp/index.jsp
@@ -9,12 +9,17 @@
   </head>
   <body>
     <h2>Hello World!</h2>
+    <br />
     <a href="/project5/test">Go to Test</a>
     <form action="/project5/test" method="post">
       <input type="text" placeholder="Enter your name" name="fullName" />
       <button type="submit">Save</button>
     </form>
+    <br />
     <a href="/project5/generic-test">Go to Generic servlet</a>
+    <br />
     <a href="/project5/interface-test">Go to Interface servlet</a>
+    <br />
+    <a href="/project5/cookies-tracking">Go to cookies-tracking</a>
   </body>
 </html>
