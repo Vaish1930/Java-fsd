13479b365f4d94b624946e4a2b9201dcf5369ec6
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-05-30 11:13:05 +0530

    Configured with xml

---
+++ project7/src/main/java/com/example/Eproduct.java
@@ -0,0 +1,57 @@
+package com.example;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "eproduct")
+public class Eproduct {
+
+    @Id
+    @GeneratedValue
+    int id;
+    String name;
+    BigDecimal price;
+    Date date_added;
+
+    public Eproduct() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Date getDate_added() {
+        return date_added;
+    }
+
+    public void setDate_added(Date date_added) {
+        this.date_added = date_added;
+    }
+
+}
--- project7/src/main/java/com/example/EproductServlet.java
+++ project7/src/main/java/com/example/EproductServlet.java
@@ -2,7 +2,7 @@ package com.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-// import java.util.List;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,20 +11,36 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-// import org.hibernate.Transaction;
+import org.hibernate.Transaction;
 
 public class EproductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            resp.setContentType("text/html");
             PrintWriter printWriter = resp.getWriter();
 
             SessionFactory factory = HibernateUtil.getSessionfactory();
 
             Session session = factory.openSession();
 
-            // Transaction transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
+
+            List<Eproduct> products = session.createQuery("from eproduct", Eproduct.class).list();
+
+            transaction.commit();
+
+            printWriter.println("<html>");
+            printWriter.println("<head><title>Hibernate Configured</title></head>");
+            printWriter.println("<body>");
+
+            printWriter.println("<b>Product Listing </b><br>");
+
+            for (Eproduct product : products) {
+                printWriter.println("Id: " + product.getId());
+                printWriter.println("Name: " + product.getName());
+                printWriter.println("Price: " + product.getPrice());
+                printWriter.println("Date Added: " + product.getDate_added());
+            }
 
             session.close();
             printWriter.println("</body></html>");
@@ -32,6 +48,7 @@ public class EproductServlet extends HttpServlet {
 
         } catch (Exception e) {
 
+            System.out.println("Error------------------------------------------------" + e.getMessage());
         }
     }
 }
