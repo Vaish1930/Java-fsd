019941eb723a66fd85f93d28a31fc829208ea6a0
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-06-08 23:27:38 +0530

    File Handling in spring-boot

---
+++ project_10/src/main/java/com/example/project_10/multipart/FileStorageException.java
@@ -0,0 +1,14 @@
+package com.example.project_10.multipart;
+
+public class FileStorageException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
