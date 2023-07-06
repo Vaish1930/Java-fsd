5af9002aa5c2ac0e9f94daba163ca708a726bccc
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-07-06 20:39:31 +0530

    Components in Angular

---
+++ project-15/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './products/product-list.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project-15';
+}
