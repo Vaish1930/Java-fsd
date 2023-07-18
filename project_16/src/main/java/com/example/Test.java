18058426ac7c1d7b6340cbba977cf5152c09640d
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-07-18 21:23:38 +0530

    completed locating elements

--- project_16/src/main/java/com/example/Test.java
+++ project_16/src/main/java/com/example/Test.java
@@ -1,12 +1,16 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get("http://127.0.0.1:5500/index.html");
+        WebElement emailElement = driver.findElement(By.id("email"));
+        System.out.println("Email by id " + emailElement);
     }
 
 }
